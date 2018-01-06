c1946b6dea817180ae5a86ca36f8d8b0e73985a0
wang-bin <wbsecg1@gmail.com>
2018-01-06 12:34:45 +0800

    std::size() returns constexpr. defined in vc140+ stl

--- include/cppcompat/iterator.hpp
+++ include/cppcompat/iterator.hpp
@@ -26,18 +26,23 @@ constexpr17 auto cend(const C& c) /*noexcep(noexcept(std::end(c)))*/
 #endif // CPP_EMU_STD(14)
 
 #if CPP_EMU_STD(17)
+# ifdef _MSC_VER // check vcrt version instead of _MSC_VER, because stl is defined in crt, and clang supports different combinations of msc version + crt 
+#   include <crtversion.h>
+# endif
+# if !defined(_MSC_VER) || (_VC_CRT_MAJOR_VERSION < 14)
 namespace std {
 template<class C> 
-constexpr17 auto size(const C& c) -> decltype(c.size())
+constexpr auto size(const C& c) -> decltype(c.size())
 {
     return c.size();
 }
 
 template<class T, std::size_t N>
-constexpr17 std::size_t size(const T (&array)[N]) noexcept
+constexpr std::size_t size(const T (&array)[N]) noexcept
 {
     return N;
 }
 } // namespace std
+# endif
 #endif // CPP_EMU_STD(17)
 
