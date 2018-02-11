b60e735d18f1c698f82615e203abd0e0267d4eb9
wang-bin <wbsecg1@gmail.com>
2018-02-11 22:54:41 +0800

    thread_local: check libc++ instead of clang version
    libc++ 4.0+ has __cxa_thread_atexit fallback. clang+libstdc++ works too

--- include/cppcompat/thread_local.hpp
+++ include/cppcompat/thread_local.hpp
@@ -1,6 +1,7 @@
 /*
- * Copyright (c) 2016-2017 WangBin <wbsecg1 at gmail.com>
+ * Copyright (c) 2016-2018 WangBin <wbsecg1 at gmail.com>
  * https://github.com/wang-bin/ThreadLocal
+ * https://github.com/wang-bin/cppcompat/blob/master/include/cppcompat/thread_local.hpp
  */
 #pragma once
 #include <functional>
@@ -16,14 +17,12 @@
 // apple: http://asciiwwdc.com/2016/sessions/405#t=354.596
 // android libc++ is poor if libc is old: llvm-libc++abi//src/cxa_thread_atexit.cpp
 #if defined(__clang__)
-# if __has_feature(cxx_thread_local) // apple clang (__apple_build_version__) for iOS(and macOS if xcode<8) has no thread_local, __thread
-#   define clang_at_least(X, Y, Z) (((__clang_major__-X)*10000000000 + (__clang_minor__-Y)*100000000 + (__clang_patchlevel__-Z)) >= 0)
-// mingw clang4 does not support non-trivial destructible type
+# if __has_feature(cxx_thread_local) // apple clang (__apple_build_version__) for iOS(and macOS if xcode<8) has no thread_local/__thread, and __has_feature(cxx_thread_local) is false, even if libc++ is 4.0+ 
+// mingw clang4 does not support non-trivial destructible type. TODO: check _LIBCPP_HAS_THREAD_API_WIN32 and _LIBCPP_HAS_THREAD_API_PTHREAD?
 #   if !(defined(_WIN32) && defined(__GNUC__)) \
-        && !(defined(__ANDROID__) && !clang_at_least(3, 0, 275480)) // supported in ndk r14 libc++, but libc++abi version is hard to check, so check clang version
+        && (!defined(_LIBCPP_VERSION) || _LIBCPP_VERSION >= 4000) // libc++abi 4.0+ add __cxa_thread_atexit fallback. libstdc++ has __cxa_thread_atexit 
 #     define CC_HAS_THREAD_LOCAL
 #   endif
-#   undef clang_at_least
 # endif
 #elif defined(_MSC_VER) && _MSC_VER >= 1900 // TODO: clang msvc supports tls
 # define CC_HAS_THREAD_LOCAL
